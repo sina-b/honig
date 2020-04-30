f883b9fc892f8685050a2cc4ffc70a1dbe577684
Sina Brandstetter <brandstetter.sina@gmail.com>
2020-04-30 15:31:19 +0200

    Add text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+
+import { ComponentsModule } from './components/components.module';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NgbdModalComponent } from './shared/order/order.component';
 import { NgbdModalContent } from './shared/order/order.component';
+import { ExamplesModule } from './examples/examples.module';
 
 import { ImageUrlService } from './services/imageUrl.service'
 
-import { ComponentsModule } from './components/components.module';
-import { ExamplesModule } from './examples/examples.module';
 import { environment } from '../environments/environment';
 
 
