d74e4d5c95f89657145c76c0cdb3f39b9565b55d
Sina Brandstetter <brandstetter.sina@gmail.com>
2020-05-02 17:18:52 +0200

    Load assets dynamically: jan, feb, mar

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-carousel',
@@ -6,39 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
-  constructor() { }
+  constructor(
+    public firestore: AngularFirestore
+  ) { }
   pauseOnHover = true;
 
-  assets_januar = [
-    ['/assets/img/januar_2015.jpg', 'Bienenkästen im Schnee'],
-    ['/assets/img/wabeneinloeten.jpg', 'Waben werden vorbereitet'],
-    ['/assets/img/januartag.jpg', 'Januar im Wald'],
-    ['/assets/img/januartag2.jpg', 'Januartag bei der Imkerei'],
-    ['/assets/img/baumschnitt.jpg', 'Bei der Imkerei werden die Bäume geschnitten'],
-    ['/assets/img/geigersbuehl.jpg', 'Großbettlingen im Januar']
-  ]
+  assets_januar = this.loadAssetList('januar')
+  assets_februar = this.loadAssetList('februar')
+  assets_maerz = this.loadAssetList('maerz')
   
-  assets_februar = [
-    ['/assets/img/jusi_winter.jpg', 'Jusi im Winter'],
-    ['/assets/img/weide_winter.jpg', 'Weide im Winter'],
-    ['/assets/img/kornellkirsche_winter.jpg', 'Kornellkirsche im Winter'],
-    ['/assets/img/huflattich.jpg', 'Huflattich'],
-    ['/assets/img/haselnuss_winter.jpg', 'Haselnuss im Winter'],
-    ['/assets/img/krokus_winter.jpg', 'Krokus im Winter'],
-    ['/assets/img/krokus_winter2.jpg', 'Blühender Krokus im Winter'],
-    ['/assets/img/wald_winter.jpg', 'Wald im Winter'],
-    ['/assets/img/hoher_wittlingen.jpg', 'Hoher Wittlingen im Schnee']
-  ]
-
-  assets_maerz = [
-    ['/assets/img/milder_maerz.jpg', 'Milder März'],
-    ['/assets/img/auswinterung.jpg', 'Die Auswinterung beginnt'],
-    ['/assets/img/anflug.jpg', 'Die Bienen fangen an zu fliegen'],
-    ['/assets/img/ende_maerz.jpg', 'Hochbetrieb Ende März'],
-    ['/assets/img/teich.jpg', 'Klotz Beute am Teich'],
-    ['/assets/img/weide_maerz.jpg', 'Weiden im März']
-  ]
-
   assets_april = [
     ['/assets/img/imkerei.jpg', 'Die Imkerei im April'],
     ['/assets/img/brutwabe_april.jpg', 'Brutwabe im April'],
@@ -151,5 +128,15 @@ export class CarouselComponent implements OnInit {
         carousels[active].classList.remove('ghost');
         });
     });
+  }
+
+  loadAssetList(month) {
+    let assetList = []
+    this.firestore.collection(month).get().toPromise().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          assetList.push([doc.data().img, doc.data().text])
+      });
+    });
+    return assetList
   }
 }
